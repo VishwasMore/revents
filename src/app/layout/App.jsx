ba736d05f20f09f42fa9b3948ec5886d5e244c95
Vishwas More <vishwasmore2019@gmail.com>
2020-01-06 16:45:37 +0400

    adding redux to app

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import { Container } from 'semantic-ui-react';
 import { Route, Switch } from 'react-router-dom';
@@ -9,6 +10,7 @@ import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard
 import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
 import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
 import EventForm from '../../features/event/EventForm/EventForm';
+import TestComponent from '../../features/testarea/TestComponent';
 
 class App extends Component {
   render() {
@@ -26,7 +28,9 @@ class App extends Component {
               <Container className="main">
                 <Switch>
                   <Route path="/events" component={EventDashboard} />
+                  <Route path="/test" component={TestComponent} />
                   <Route path="/event/:id" component={EventDetailedPage} />
+                  <Route path="/manage/:id" component={EventForm} />
                   <Route path="/people" component={PeopleDashboard} />
                   <Route path="/profile/:id" component={UserDetailedPage} />
                   <Route path="/settings" component={SettingsDashboard} />
--- src/index.js
+++ src/index.js
@@ -2,61 +2,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 // import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
+import { configureStore } from './app/store/configureStore';
+import ScrollToTop from './app/common/utils/ScrollToTop';
 
+
+const store = configureStore();
 
 const rootEl = document.getElementById('root');
+
 let render = () => {
-    ReactDOM.render(
+  ReactDOM.render(
+    <Provider store={store}>
       <BrowserRouter>
-        <App />
-      </BrowserRouter>,
-      rootEl
-    );
-  };
-  
-  if (module.hot) {
-    module.hot.accept('./app/layout/App', () => {
-      setTimeout(render);
-    });
-  }
-  
-  render();
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
+      </BrowserRouter>
+    </Provider>,
+    rootEl
+  );
+};
+
+if (module.hot) {
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render);
+  });
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { BrowserRouter } from 'react-router-dom';
-// import 'semantic-ui-css/semantic.min.css';
-// import './index.css';
-// import App from './app/layout/App';
-// import registerServiceWorker from './registerServiceWorker';
-
-// const rootEl = document.getElementById('root');
-
-// let render = () => {
-//   ReactDOM.render(
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>,
-//     rootEl
-//   );
-// };
-
-// if (module.hot) {
-//   module.hot.accept('./app/layout/App', () => {
-//     setTimeout(render);
-//   });
-// }
-
-// render();
-
-// registerServiceWorker();
